e28c4755f529636f319d36b5dd2988fd1c7b47b8
Alastair Davis <ali.dee1410@gmail.com>
2025-04-04 19:40:27 +0100

    Accept popup max width

--- globals/dialog/popups/accept_popup.gd
+++ globals/dialog/popups/accept_popup.gd
@@ -1,6 +1,14 @@
 class_name AcceptPopup extends DialogPopup
 
+@export var max_width: float = 1000
 @export var message_lbl: Label
+
+func _ready() -> void:
+	if message_lbl.size.x < max_width:
+		message_lbl.autowrap_mode = TextServer.AUTOWRAP_OFF
+	else:
+		message_lbl.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+		custom_minimum_size.x = max_width
 
 func _on_accept_btn_pressed() -> void:
 	close()
