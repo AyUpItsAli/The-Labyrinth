754ed8dc593698e688eb64a19f2cb00bea99e117
Alastair Davis <ali.dee1410@gmail.com>
2025-07-14 23:38:09 +0100

    Chat panel saved as separate scene and added to game UI

--- scenes/screens/lobby/lobby.gd
+++ scenes/screens/lobby/lobby.gd
@@ -1,48 +1,19 @@
 extends Control
 
-const CHAT_MESSAGE = preload("res://scenes/ui/chat/chat_message.tscn")
-
 @export var lobby_name_lbl: Label
 @export var invite_btn: Button
 @export var member_count_lbl: Label
-@export var chat_container: ScrollContainer
-@export var message_container: Container
 @export var message_edit: LineEdit
 @export var player_list: ItemList
 @export var start_btn: Button
 
 func _ready() -> void:
-	Network.chat_updated.connect(update_chat)
 	Network.players_updated.connect(update_players)
 	lobby_name_lbl.set_text(Network.get_lobby_data("name", "Untitled Lobby"))
 	invite_btn.set_visible(multiplayer.is_server())
 	message_edit.grab_focus()
-	update_chat()
 	update_players()
 	Overlay.finish_loading()
-
-func update_chat() -> void:
-	for child in message_container.get_children():
-		message_container.remove_child(child)
-		child.queue_free()
-	for msg: Dictionary in Network.chat:
-		var message: ChatMessage = CHAT_MESSAGE.instantiate()
-		match msg.get("type"):
-			Network.MessageType.NETWORK:
-				message.content_lbl.set_text("[center]%s[/center]" % msg.get("content"))
-				message.timestamp_lbl.hide()
-			_:
-				message.content_lbl.set_text(msg.get("content"))
-				var bias: int = Time.get_time_zone_from_system().get("bias")
-				var unix_time: int = msg.get("time") + (bias * 60)
-				var time: Dictionary = Time.get_time_dict_from_unix_time(unix_time)
-				message.timestamp_lbl.set_text("%02d:%02d" % [time.get("hour"), time.get("minute")])
-		message_container.add_child(message)
-	# No idea why we need to wait 2 frames, instead of 1, before setting scroll
-	await get_tree().process_frame
-	await get_tree().process_frame
-	# Scroll to end
-	chat_container.set_v_scroll(Vector2i.MAX.x)
 
 func update_players() -> void:
 	# Update lobby title with new player count
@@ -63,10 +34,6 @@ func update_players() -> void:
 
 func _on_invite_btn_pressed() -> void:
 	Overlay.display_invite_popup()
-
-func _on_message_edit_text_submitted(content: String) -> void:
-	message_edit.clear()
-	Network.send_player_message(content)
 
 func _on_start_btn_pressed() -> void:
 	if multiplayer.is_server():
---
+++ scenes/ui/chat/chat.gd
@@ -0,0 +1,40 @@
+class_name Chat extends PanelContainer
+
+const CHAT_MESSAGE = preload("res://scenes/ui/chat/chat_message.tscn")
+
+@export var message_edit: LineEdit
+@export_group("Nodes")
+@export var scroll_container: ScrollContainer
+@export var message_container: Container
+
+func _ready() -> void:
+	Network.chat_updated.connect(refresh)
+	message_edit.text_submitted.connect(_on_message_edit_text_submitted)
+	refresh()
+
+func refresh() -> void:
+	for child in message_container.get_children():
+		message_container.remove_child(child)
+		child.queue_free()
+	for msg: Dictionary in Network.chat:
+		var message: ChatMessage = CHAT_MESSAGE.instantiate()
+		match msg.get("type"):
+			Network.MessageType.NETWORK:
+				message.content_lbl.set_text("[center]%s[/center]" % msg.get("content"))
+				message.timestamp_lbl.hide()
+			_:
+				message.content_lbl.set_text(msg.get("content"))
+				var bias: int = Time.get_time_zone_from_system().get("bias")
+				var unix_time: int = msg.get("time") + (bias * 60)
+				var time: Dictionary = Time.get_time_dict_from_unix_time(unix_time)
+				message.timestamp_lbl.set_text("%02d:%02d" % [time.get("hour"), time.get("minute")])
+		message_container.add_child(message)
+	# No idea why we need to wait 2 frames, instead of 1, before setting scroll
+	await get_tree().process_frame
+	await get_tree().process_frame
+	# Scroll to end
+	scroll_container.set_v_scroll(Vector2i.MAX.x)
+
+func _on_message_edit_text_submitted(content: String) -> void:
+	message_edit.clear()
+	Network.send_player_message(content)
